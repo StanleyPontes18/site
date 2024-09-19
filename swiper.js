10e6e00e3f5b60a7b14eb528ac3592ed7b6c5fed
StanleyPontes18 <vloggerstanley@gmail.com>
2024-09-19 10:51:16 -0300

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,5 @@
+<!-- CSS do Swiper -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/swiper/8.0.7/swiper-bundle.min.css" />
+
+<!-- JS do Swiper -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/swiper/8.0.7/swiper-bundle.min.js"></script>
